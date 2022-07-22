c4647a495bbb0e69b6d16de41e9fb8e126e48f06
choudel <chouaibh@gmail.com>
2022-07-22 17:16:24 +0100

    variables shadowing and const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let popsssy= 5;
+    println!("HELLLLLLO the number is: {}",popsssy);
+    {
+        let popsssy= 6;
+    println!("HELLLLLLO the number is: {}",popsssy);
+    
+    }
+    let popsssy= popsssy+ 5;
+    println!("HELLLLLLO the number is: {}",popsssy);
+    
+const POPY: u64=63;
+println!("pond op ber is: {}",POPY);
 }
