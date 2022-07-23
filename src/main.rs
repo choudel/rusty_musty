049a85b9ba49b442e65b119bb77f2d297d9e42c9
choudel <chouaibh@gmail.com>
2022-07-23 03:41:59 +0100

    casting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
 fn main() {
-    let mut input= String::new();
-
-    io::stdin().read_line(&mut input).expect("failed to read line");
-    println!("{}",input);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("there is a problem");
+    let int_input: i64 = input.trim().parse().unwrap();
+    println!("the raw input is : {}", input);
+    println!("the converted input is : {}", int_input + 2);
 }
