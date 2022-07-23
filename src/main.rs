f1be6ba7ffabb9ba2ef6325d42778115b12bc639
choudel <chouaibh@gmail.com>
2022-07-23 06:01:15 +0100

    expressions and statements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::io;
 fn main() {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("there is a problem");
-    let int_input: i64 = input.trim().parse().unwrap();
-    println!("the raw input is : {}", input);
-    println!("the converted input is : {}", int_input + 2);
+    println!("hello world");
+    test_one();
+    test_one();
+    add_numbers(3, 4);
+
+    let number = {
+        let x = 3;
+        x + 1 //don't add a semicolon
+    };
+    println!("{}", number);
+    println!("{}", add_numbers2(12, 13));
+}
+
+fn test_one() {
+    println!("test has been called ...");
+}
+fn add_numbers(x: i32, y: i32) {
+    println!("the sum is: {}", x + y);
+}
+fn add_numbers2(x: i32, y: i32) -> i32 {
+    x + y
 }
