f02a0da3cfdde8b4afc5e35384d4d261b718a3f5
choudel <chouaibh@gmail.com>
2022-07-24 10:07:39 +0100

    pas by refrence

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-struct Color(u8,u8,u8);
+struct Color{
+    red:u8,
+    blue:u8,
+    green:u8
+}
 
 fn main() {
-    let mut red = Color(255,0,6);
-    red.2=60;
-    println!("red is {},{},{}", red.0, red.1, red.2);
+    let blue = Color{red:2,green:41,blue:69};
+    print_color(&blue);
+    print_color(&blue);
+}
+fn print_color(c:&Color) {
+    println!("Color R:{} G:{} B:{}",c.red, c.green,c.blue)
 }
