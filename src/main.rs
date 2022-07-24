0eb527b8a495918c7092fbb6e7c486fd67ce28a9
choudel <chouaibh@gmail.com>
2022-07-24 16:18:59 +0100

    Implementing traits

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-fn main() {
-    let mut my_string = String::from("helllo man banner");
-
-    println!("Length:{}", my_string.len());
-    println!("String is empty ? {}", my_string.is_empty());
-
-    for token in my_string.split_whitespace() {
-        println!("//========>{}<========//", token);
+struct Person {
+    name: String,
+    age: u8,
+}
+impl ToString for Person {
+    fn to_string(&self) -> String {
+        return format!("My name is {} and I am {}", self.name, self.age);
     }
-    println!(
-        "Does the string contain 'man'? {} ",
-        my_string.contains("man")
-    );
-    my_string.push_str(" Welcome to strings!");
-    println!("Same thing: {}", my_string);
+}
+fn main() {
+    let dom = Person {
+        name: String::from("Choudel"),
+        age: 21,
+    };
+
+    println!("{}", dom.to_string());
 }
