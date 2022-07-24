8f9cb6d9d92ce5c680cfcc6dd31103400101f849
choudel <chouaibh@gmail.com>
2022-07-24 08:34:25 +0100

    refrences

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 fn main() {
-    print_numbers_to(10);
-}
-
-fn print_numbers_to(num: u32) {
-    for n in 1..num {
-        if is_even(n) {
-            println!("{} is even!", n);
-        } else {
-            println!("{} is odd", n);
-        }
+    let mut x = 10;
+    {
+        let dom = &mut x;
+        *dom += 1;
     }
-}
-fn is_even(num: u32) -> bool {
-    return num % 2 == 0;
+    println!("x is {}", x);
 }
