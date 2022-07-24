925211a1982ba392b185226770c6a71042694ad4
choudel <chouaibh@gmail.com>
2022-07-24 04:28:38 +0100

    the for loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
-    let mut n = 0;
-    while n <= 50 {
-        if n % 5 == 0 {
-            println!("n is {}--------------", n);
-        } else {
-            println!("n is {}", n);
-        }
-
-        n += 1;
+    let animals = vec!["Rabbit","hnech","klebs"];
+    for (index,a) in animals.iter().enumerate(){
+        println!("the index is : {} and the animal is {}",index,a);
     }
 }
