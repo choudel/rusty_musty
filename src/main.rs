2e7249a046790c9aad2023885335b459350ef321
choudel <chouaibh@gmail.com>
2022-07-24 09:35:34 +0100

    struct tuple

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
+struct Color(u8,u8,u8);
+
 fn main() {
-    let bg = Color {
-        blue: 21,
-        green: 85,
-        red: 63,
-    };
-    println!("{},{},{}", bg.blue, bg.green, bg.red);
+    let mut red = Color(255,0,6);
+    red.2=60;
+    println!("red is {},{},{}", red.0, red.1, red.2);
 }
