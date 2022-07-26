8c5013d5d5d6529ee4fa9ac55f7716dec3fb64d0
choudel <chouaibh@gmail.com>
2022-07-26 12:08:14 +0100

    HashMaps

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::io;
+use std::collections::HashMap;
 fn main() {
-    let mut input = String::new();
-    println!("Hey mate! Say something:");
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {
-            println!("Success! You said: {}", input);
-        }
-        Err(e) => println!("Ooops! Something went wrong : {}", e)
+    let mut marks = HashMap::new();
+
+    marks.insert("Rust programming", 96);
+    marks.insert("Web Development", 94);
+    marks.insert("UX Design", 75);
+    marks.insert("Professional Computing Studies", 45);
+
+    println!("How many subjects have you stadied? {}", marks.len());
+    match marks.get("Web Development") {
+        Some(mark) => println!("you got {} for Web Dev!", mark),
+        None => println!("You didn't study that LOL"),
     }
+    marks.remove("UX Design");
+    for (subject, mark) in &marks {
+        println!("For {} you got {}%!",subject,mark);
+    } 
+    println!("did you study C++? {}",marks.contains_key("C++ Programming"));
 }
