dcb0dd6adead7fc10ab52032e932e6a7f4b3e406
choudel <chouaibh@gmail.com>
2022-07-26 06:47:04 +0100

    command line args

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Person {
-    name: String,
-    age: u32,
-}
-impl Person {
-    pub fn new(name: String, age: u32) -> Self {
-        Person { name, age }
-    }
-}
+use std::env;
+
 fn main(){
-    let mut people =vec![
-        Person::new("Al".to_string(),25),
-        Person::new("John".to_string(),3),
-        Person::new("Zoe".to_string(), 23)
-    ];
-    people.sort_by(|a,b| b.age.cmp(&a.age));
-    assert_eq!(
-        people,
-        vec![
-            Person::new("Al".to_string(),25),
-            Person::new("Zoe".to_string(), 23),
-            Person::new("John".to_string(),3),
-        ]
-    );
-    println!("Success");
+    let args: Vec<String>=env::args().collect();
+    for arguments in args.iter(){
+        println!("{}",arguments);
+    }
 }
