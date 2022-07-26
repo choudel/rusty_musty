6c48eddd3ba35b1c99ca6fba264b9a8140f2f024
choudel <chouaibh@gmail.com>
2022-07-26 08:06:49 +0100

    traits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
-use std::fs::File;
-use std::io::prelude::*;
-
-fn main(){
-    let mut file = File::create("output.txt").expect("Could not create file!");
-    file.write_all(b"Welcome to my life ").expect("something went wrong");            
+struct Person {
+    name: String,
+    age: u8,
+}
+struct Child {
+    name: String,
+    age: u8,
+}
+trait HasVoiceBox {
+    fn speak(&self);
+    fn can_speak(&self) -> bool;
+}
+impl HasVoiceBox for Person {
+    fn speak(&self) {
+        println!("Hello,my name is {}", self.name);
+    }
+    fn can_speak(&self) -> bool {
+        if self.age > 3 {
+            return true;
+        }
+        return false;
+    }
+}
+impl HasVoiceBox for Child {
+    fn speak(&self) {
+        println!("I'm a child and my name is {}", self.name)
+    }
+    fn can_speak(&self) -> bool {
+        if self.age > 0 && self.age < 3 {
+            return false;
+        }
+        return true;
+    }
+}
+fn main() {
+    let person = Person {
+        name: String::from("choudel"),
+        age: 41,
+    };
+    println!("Can flen '{}' speak? {}", person.name, person.can_speak());
+    person.speak();
+    let baby = Child {
+        name: String::from("pitbull"),
+        age: 2,
+    };
+    println!(
+        "can this baby talk? : {}, literaly: {}",
+        baby.name, baby.can_speak()
+    );
+    baby.speak()
 }
