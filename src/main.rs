a17ed65c80eba97cc01f6d5f87e72381cbf9552b
choudel <chouaibh@gmail.com>
2022-07-26 11:22:30 +0100

    input in command line

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,11 @@
-struct Person {
-    name: String,
-    age: u8,
-}
-struct Child {
-    name: String,
-    age: u8,
-}
-trait HasVoiceBox {
-    fn speak(&self);
-    fn can_speak(&self) -> bool;
-}
-impl HasVoiceBox for Person {
-    fn speak(&self) {
-        println!("Hello,my name is {}", self.name);
-    }
-    fn can_speak(&self) -> bool {
-        if self.age > 3 {
-            return true;
-        }
-        return false;
-    }
-}
-impl HasVoiceBox for Child {
-    fn speak(&self) {
-        println!("I'm a child and my name is {}", self.name)
-    }
-    fn can_speak(&self) -> bool {
-        if self.age > 0 && self.age < 3 {
-            return false;
-        }
-        return true;
-    }
-}
+use std::io;
 fn main() {
-    let person = Person {
-        name: String::from("choudel"),
-        age: 41,
-    };
-    println!("Can flen '{}' speak? {}", person.name, person.can_speak());
-    person.speak();
-    let baby = Child {
-        name: String::from("pitbull"),
-        age: 2,
-    };
-    println!(
-        "can this baby talk? : {}, literaly: {}",
-        baby.name, baby.can_speak()
-    );
-    baby.speak()
+    let mut input = String::new();
+    println!("Hey mate! Say something:");
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("Success! You said: {}", input);
+        }
+        Err(e) => println!("Ooops! Something went wrong : {}", e)
+    }
 }
