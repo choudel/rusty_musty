2024d62d82fa79e376542e6fed85b9bbfd76fd81
choudel <chouaibh@gmail.com>
2022-07-29 07:58:22 +0100

    fmt macro implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use std::fmt;
+#[derive(Debug)]
 struct Object {
     width: u32,
     height: u32,
 }
+//Methods
 impl Object {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    
+    fn show(&self) {
+        println!("{}and{} the area is: {}", self.width, self.height, self.area());
+    }
+}
+impl Object {
     fn new (width:u32, height:u32)->Object{
         Object { width, height }
     }
-    fn show(&self) {
-        println!("{}and{} the area is: {}", self.width, self.height, self.area());
+}
+impl fmt::Display for Object{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"({},{})and Area: {}",self.height,self.width,self.area())
     }
 }
 
@@ -19,4 +30,5 @@ fn main() {
     let obj = Object::new(15, 60);
     
     obj.show();
+    println!("{}",obj)
 }
