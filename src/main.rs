d7a4c363ebcb27450ca1d8712a6e950841cd0b03
choudel <chouaibh@gmail.com>
2022-08-02 07:42:13 +0200

    first sample

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
-use std::fmt;
-#[derive(Debug)]
-struct Object {
-    width: u32,
-    height: u32,
-}
-//Methods
-impl Object {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    
-    fn show(&self) {
-        println!("{}and{} the area is: {}", self.width, self.height, self.area());
-    }
-}
-impl Object {
-    fn new (width:u32, height:u32)->Object{
-        Object { width, height }
-    }
-}
-impl fmt::Display for Object{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"({},{})and Area: {}",self.height,self.width,self.area())
-    }
-}
-
+#![allow(unused)]
+use std::io;
+use rand::Rng;
+use std::io::{Write,BufReader,BufRead,ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
 fn main() {
-    
-    let obj = Object::new(15, 60);
-    
-    obj.show();
-    println!("{}",obj)
+  println!("what is your name ?");
+  let mut name=String::new();
+  let greeting="nice to meet you";
+  io::stdin().read_line(&mut name).expect("didn't recieve input");
+  println!("Hello {}! {}", name.trim_end(),greeting)
 }
