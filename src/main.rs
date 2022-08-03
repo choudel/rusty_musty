4856a2594b3224f31df1ff2ebdb4c54a78fe20c0
choudel <chouaibh@gmail.com>
2022-08-03 10:54:54 +0200

    string methods

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,31 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 fn main() {
-    let num_1: f32 = 1.11111111111;
-    println!("f32:{}", num_1 + 0.111111111111111);
-    let random_num = rand::thread_rng().gen_range(5..25);
-    println!("{}", random_num);
-    let my_age = 47;
-    let can_vote = if my_age >= 18 { true } else { false };
-    println!("can vote: {}", can_vote);
-    let voting_age = 18;
-    match my_age.cmp(&voting_age) {
-        Ordering::Less => println!("can't vote"),
-        Ordering::Greater => println!("you can vote"),
-        Ordering::Equal => println!(" you just made it to vote"),
-    }
+  let mut st1=String::new();
+  st1.push('A');
+  st1.push_str("word");
+  for word in st1.split_whitespace(){
+    println!("{}",word);
+  }
+  let st2= st1.replace("A", "Another ");
+  println!("{}",st2);
+  let st3 = String::from("X v H f u IO u i");
+  let mut v1: Vec<char> = st3.chars().collect();
+  v1.sort();
+  v1.dedup();
+  for char in v1 {
+      println!("{}",char);
+  }
+  let st4:&str="Random string";
+  let mut st5:String=st4.to_string();
+  println!("{}",st5);
+  let byte_arr1 = st5.as_bytes();
+  let st6 = &st5[1..4];
+  println!("{}",st6);
+  let st6 = String::from("Just some");
+  let st7=String::from("words ");
+  let st8 =st6+&st7;
+  for char in st8.bytes(){
+    println!("{}",char)
+  }
 }
